abd62f3463ebb6691d27cfa4b583688f38c077d9
QAREKEN <kalabaevshj@gmail.com>
2018-04-24 05:07:19 +0500

    Added ImageModel, image_item.xml and RecyclerView to mMainActivity

--- app/src/main/java/com/dasturlash/gallery_android/main_screen/MainActivity.java
+++ app/src/main/java/com/dasturlash/gallery_android/main_screen/MainActivity.java
@@ -1,7 +1,9 @@
-package com.dasturlash.gallery_android;
+package com.dasturlash.gallery_android.main_screen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.dasturlash.gallery_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
