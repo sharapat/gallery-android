c1e751be8874a28b5c2ef42da15a2deaaf29975a
QAREKEN <kalabaevshj@gmail.com>
2018-05-05 03:17:08 +0500

    Added searchView and zoom image view

--- app/src/main/java/com/dasturlash/gallery_android/mainscreen/MainActivity.java
+++ app/src/main/java/com/dasturlash/gallery_android/mainscreen/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dasturlash.gallery_android.mainscreen;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.annotation.NonNull;
@@ -8,6 +10,11 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.dasturlash.gallery_android.R;
 import com.dasturlash.gallery_android.ResponseHolder;
@@ -46,8 +53,29 @@ public class MainActivity extends AppCompatActivity implements GalleryListener {
             getData();
         } else {
             photosModel = ResponseHolder.getInstance().getPhotosModel();
+            adapter.updateModel(photosModel.getPhotos().getPhoto());
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        assert searchManager != null;
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnSearchClickListener(searchListener);
+        return true;
+    }
+
+    View.OnClickListener searchListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(MainActivity.this, "search clicked", Toast.LENGTH_LONG).show();
+        }
+    };
 
     public void getData() {
         ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
